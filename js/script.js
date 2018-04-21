fce413b1c24efd9fe26dec801413de8a8eb312c0
BogdaholaS <bogdan20022404@gmail.com>
2018-04-21 19:47:10 +0300

    Почав писати js для фільтру

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// let search = () => {
+// 	let input , filter , div , item , a , i;
+// 	input = document.getElementById('keywords')
+// 	filter = input.value.toUpperCase()
+// 	div = document.getElementById('items')
+// 	item = document.getElementById('item')
+
+// 	for (var i = 0; i < item.length; i++) {
+// 		a = item[i].getElementsByTagName('a')[0]
+// 		if (a.innerHTML.toUpperCase(),indexOf(filter) > -1){
+// 			item[i].style.display = '';
+// 		}
+// 		else{
+// 			item[i].style.display = 'none'
+// 		}
+// 	}
+// }
+
+let search = () => {
+	
+}
